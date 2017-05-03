105a5eebc8f4f9055e51721a050c034cc6b8eeaf
chenr-maxim <chenr.maxim@gmail.com>
2017-05-03 12:55:04 -0700

    asdfasd

---
+++ TutorNow/src/pages/client-summary/client-summary.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Platform, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-client-summary',
+  templateUrl: 'client-summary.html'
+})
+export class ClientSummaryPage {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private platform:Platform) {
+
+  }
+
+}
