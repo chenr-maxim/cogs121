9259afd7ffd21de21c96ca3f9ca595d38f03dbfa
Max Chen <chenr.maxim@gmail.com>
2017-05-09 16:32:15 -0700

    meteor -> account settings functionality

--- TutorNow/src/pages/provider-profile/provider-profile.ts
+++ TutorNow/src/pages/provider-profile/provider-profile.ts
@@ -27,11 +27,10 @@ export class ProviderProfilePage {
   private classesText = '';
   classes = []
   options = [
-    {
-    name:'Tutoring', value:'1', checked:false},
-    {name:'Lessons', value:'2', checked:false},
-    {name:'Tour Guide', value:'3', checked:false},
-    {name:'Miscellaneous', value:'4', checked:false}
+    {type:'Tutoring', value:'1', checked:false},
+    {type:'Lessons', value:'2', checked:false},
+    {type:'Tour Guide', value:'3', checked:false},
+    {type:'Miscellaneous', value:'4', checked:false}
   ]
 
 
